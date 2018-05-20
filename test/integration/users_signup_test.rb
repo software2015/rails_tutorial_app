3812606f97e0b41d30365af15bcfa116c6a41851
software2015 <hhll69241@gmail.com>
2018-05-20 15:52:27 +0300

    Finish user signup

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+	test "invalid signup information" do
+		get signup_path
+
+		assert_no_difference 'User.count' do
+			post signup_path, params: { user: { name: "alexander", email:"alexanderalexander.alexander", password: "alexander", password_confirmation: "alexander" }}
+		end
+
+		assert_template 'shared/_error_messages'
+		assert_select 'form[action="/signup"]'
+#		assert_select 'div#error_explanation'
+#		assert_select 'div.alert alert-danger'
+
+	end
+
+	test "valid signup information" do
+		get signup_path
+
+		assert_difference 'User.count', 1 do
+			post signup_path, params: { user: { name: "alexander", email:"alexander@alexander.alexander", password: "alexander", password_confirmation: "alexander" }}
+		end
+
+		follow_redirect!
+		assert_template 'users/show'
+		assert_not flash.empty?
+#		assert_select 'div#error_explanation'
+#		assert_select 'div.alert alert-danger'
+
+	end
+
+end
